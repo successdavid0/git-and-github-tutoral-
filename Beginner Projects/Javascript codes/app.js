2911d31b1da924dd0323115d71e1a21effacc9d7
success david <successdavidpraise99@gmail.com>
2025-07-03 13:15:31 -0700

    the new folder

---
+++ Beginner Projects/Javascript codes/app.js
@@ -0,0 +1,16 @@
+// let userAge = 18
+
+// if(userAge > 18 ){
+//     console.log(`your ${userAge} years old you may enter the club`)
+// }
+// else if(userAge === 18){
+//     console.log(`you are ${userAge} years old you may enter the app`)
+// }
+// else{
+//     console.log(`you are ${userAge} years old you are too young for this club`)
+// }
+
+
+let counter = 1
+
+while(counter )
