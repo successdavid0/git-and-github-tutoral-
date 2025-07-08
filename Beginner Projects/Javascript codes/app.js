1c5d39c85a553412206a51d6583d657d3b0ec60b
success david <successdavidpraise99@gmail.com>
2025-07-08 13:50:23 -0700

    projects i have been working on but never decided to post on git hub

--- Beginner Projects/Javascript codes/app.js
+++ Beginner Projects/Javascript codes/app.js
@@ -1,43 +1,36 @@
-// let userAge = 18
-
-// if(userAge > 18 ){
-//     console.log(`your ${userAge} years old you may enter the club`)
-// }
-// else if(userAge === 18){
-//     console.log(`you are ${userAge} years old you may enter the app`)
-// }
-// else{
-//     console.log(`you are ${userAge} years old you are too young for this club`)
-// }
-
-
-// let counter = 1
-
-// while(counter <= 20){
-//     console.log(counter)
-//     counter = counter +1
-// }
-
-// let str = "Asap Frontend";
-
-// for (let i = 1; i <= 15; i++) {
-    // console.log(i)
-//     if(i % 5 === 0){
-//         console.log("Asap Frontend")
-//     }
-//     else if (i%2 === 0){
-//         console.log("Frontend")
-//     }
-//     else if(i % 2 === 1){
-//         console.log("Asap")
-//     }
-// }
-
-function main(name) {
-    console.log(name)
-
-    console.log(name)
+//Regular function 
+function main(USD) {
+    const AUD = USD * 1.5
+    return(AUD)
 }
+
+console.log(main(1000))
 //trying to abapt to things properly 
 
-main("hello world")
+
+//arrow fucntion 
+const convert = (dollars) => {
+    return dollars * 1.5;
+}
+
+console.log(convert(1000))
+
+//arrays
+let array = ["apple", "mango", "eggs", "milk","water"]
+
+//getting the last element in an array or list 
+console.log(array[array.length - 1])
+
+// adding to array (array methods)
+array.push("bread")
+
+console.log(array)
+//second array method .filter
+array.filter((element) => {
+    console.log(element);
+    if (element.length >== 4 ){
+        return true
+    }
+
+})
+
